b34b6759411e7e5e38932b66748e1a17d08e1b93
Igor Krashenyi <reachingxforward@gmail.com>
2024-02-01 10:33:37 +0200

    Update transforms.py

--- ttach/transforms.py
+++ ttach/transforms.py
@@ -59,7 +59,7 @@ class VerticalFlip(DualTransform):
 
 
 class ChannelFlip(DualTransform):
-    """Flip images vertically (up->down)"""
+    """Flip images channel-wise (up->down)"""
 
     identity_param = False
 
@@ -103,6 +103,99 @@ class Rotate90(DualTransform):
     def apply_aug_image(self, image, angle=0, **kwargs):
         k = angle // 90 if angle >= 0 else (angle + 360) // 90
         return F.rot90(image, k)
+
+    def apply_deaug_mask(self, mask, angle=0, **kwargs):
+        return self.apply_aug_image(mask, -angle)
+
+    def apply_deaug_label(self, label, angle=0, **kwargs):
+        return label
+
+    def apply_deaug_keypoints(self, keypoints, angle=0, **kwargs):
+        angle *= -1
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.keypoints_rot90(keypoints, k=k)
+
+
+class Rotate90ch(DualTransform):
+    """Rotate images 0/90/180/270 degrees
+
+    Args:
+        angles (list): angles to rotate images
+    """
+
+    identity_param = 0
+
+    def __init__(self, angles: List[int]):
+        if self.identity_param not in angles:
+            angles = [self.identity_param] + list(angles)
+
+        super().__init__("angle", angles)
+
+    def apply_aug_image(self, image, angle=0, **kwargs):
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.rot90ch(image, k)
+
+    def apply_deaug_mask(self, mask, angle=0, **kwargs):
+        return self.apply_aug_image(mask, -angle)
+
+    def apply_deaug_label(self, label, angle=0, **kwargs):
+        return label
+
+    def apply_deaug_keypoints(self, keypoints, angle=0, **kwargs):
+        angle *= -1
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.keypoints_rot90(keypoints, k=k)
+
+
+class Rotate90hv(DualTransform):
+    """Rotate images 0/90/180/270 degrees
+
+    Args:
+        angles (list): angles to rotate images
+    """
+
+    identity_param = 0
+
+    def __init__(self, angles: List[int]):
+        if self.identity_param not in angles:
+            angles = [self.identity_param] + list(angles)
+
+        super().__init__("angle", angles)
+
+    def apply_aug_image(self, image, angle=0, **kwargs):
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.rot90hv(image, k)
+
+    def apply_deaug_mask(self, mask, angle=0, **kwargs):
+        return self.apply_aug_image(mask, -angle)
+
+    def apply_deaug_label(self, label, angle=0, **kwargs):
+        return label
+
+    def apply_deaug_keypoints(self, keypoints, angle=0, **kwargs):
+        angle *= -1
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.keypoints_rot90(keypoints, k=k)
+
+
+class Rotate90cv(DualTransform):
+    """Rotate images 0/90/180/270 degrees
+
+    Args:
+        angles (list): angles to rotate images
+    """
+
+    identity_param = 0
+
+    def __init__(self, angles: List[int]):
+        if self.identity_param not in angles:
+            angles = [self.identity_param] + list(angles)
+
+        super().__init__("angle", angles)
+
+    def apply_aug_image(self, image, angle=0, **kwargs):
+        k = angle // 90 if angle >= 0 else (angle + 360) // 90
+        return F.rot90cv(image, k)
 
     def apply_deaug_mask(self, mask, angle=0, **kwargs):
         return self.apply_aug_image(mask, -angle)
